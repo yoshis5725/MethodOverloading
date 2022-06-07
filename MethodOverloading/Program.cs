22e5a010ce36594879ddb5ef2d4fbe8e9a298fb8
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-06 21:26:03 -0600

    completed assignment

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+
+        public static void Add(int num1, int num2, bool isTrue)
+        {
+            var statement = isTrue == true ? $"{num1 + num2} dollars" : "0 dollars";
+            Console.WriteLine(statement);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Add(2, 4);
+            Add(5, 5, true);
+        }
+    }
+}
+
